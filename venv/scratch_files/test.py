11e23e48ec1dd9e762d94dd86dd2dc140b1ffefb
vixinxiviir <cody.r.byers@gmail.com>
2024-01-11 16:52:45 -0700

    Added jsons folder and achievement index table to data model

--- venv/scratch_files/test.py
+++ venv/scratch_files/test.py
@@ -4,24 +4,32 @@ from blizzardapi import BlizzardApi
 from dotenv import dotenv_values
 
 
-def getWowCategoriesIndex(clientId, clientSecret):
+def getWowAchievementCategoriesIndex(clientId, clientSecret):
     apiClient = BlizzardApi(client_id=clientId, client_secret=clientSecret)
     categories = apiClient.wow.game_data.get_achievement_categories_index(region="us", locale="en_US")
     rawJson = json.dumps(categories, indent=4)
-    with open("wowCategoriesIndex.json", "w") as outfile:
+    with open("venv/raw_jsons/wowAchievementCategoriesIndex.json", "w") as outfile:
         outfile.write(rawJson)
 
-def getWowCategory(clientId, clientSecret, categoryId):
+def getWowAchievementCategory(clientId, clientSecret, categoryId):
     apiClient = BlizzardApi(client_id=clientId, client_secret=clientSecret)
     categories = apiClient.wow.game_data.get_achievement_category(region="us", locale="en_US", achievement_category_id=categoryId)
     rawJson = json.dumps(categories, indent=4)
-    with open("wowCategories%s.json" % categoryId, "w") as outfile:
+    with open("venv/raw_jsons/wowAchievementCategories%s.json" % categoryId, "w") as outfile:
+        outfile.write(rawJson)
+
+def getWowAchievementIndex(clientId, clientSecret):
+    apiClient = BlizzardApi(client_id=clientId, client_secret=clientSecret)
+    categories = apiClient.wow.game_data.get_achievements_index(region="us", locale="en_US")
+    rawJson = json.dumps(categories, indent=4)
+    with open("venv/raw_jsons/wowAchievementIndex.json", "w") as outfile:
         outfile.write(rawJson)
 
 config = dotenv_values("venv/config/.env")
 clientId = config["CLIENT_ID"]
 clientSecret = config["CLIENT_SECRET"]
 
-getWowCategoriesIndex(clientId, clientSecret)
-getWowCategory(clientId, clientSecret, categoryId=14922)
+getWowAchievementCategoriesIndex(clientId, clientSecret)
+getWowAchievementCategory(clientId, clientSecret, categoryId=14922)
+getWowAchievementIndex(clientId, clientSecret)
 print("Request success!")
